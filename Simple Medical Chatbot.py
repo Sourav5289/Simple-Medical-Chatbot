319efd8a7c8bf273199c4dc9cf1870ff3cbdc51f
Sourav HM <souravhm5289@gmail.com>
2026-02-20 09:50:26 +0530

    Update Simple Medical Chatbot.py

--- Simple Medical Chatbot.py
+++ Simple Medical Chatbot.py
@@ -8,6 +8,13 @@ medical_db = {
         "causes": ["Stress", "Tension", "Migraine", "Dehydration", "Eye strain"],
         "remedies": ["Rest in a quiet room", "Apply cold compress", "Take OTC pain relievers", "Stay hydrated"]
     },
+"corns": {
+
+"description": "Corns are thickened skin areas due to
+
+friction.",
+
+"causes": ["Tight footwear"], "remedies": ["Proper shoes", "Foot care"]},
 "warts": {
 
 "description": "Warts are small skin growths caused by HPV virus.",
