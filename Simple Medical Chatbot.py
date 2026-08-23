9b9c6e412d6e9406d17b494942ff964e7624018f
Sourav HM <souravhm5289@gmail.com>
2026-08-23 09:40:06 +0530

    Update Simple Medical Chatbot.py

--- Simple Medical Chatbot.py
+++ Simple Medical Chatbot.py
@@ -12,7 +12,7 @@ medical_db = {
   "causes": ["Severe depression", "Psychotic disorders", "Neurological disease"],
   "remedies": ["Antidepressants", "Antipsychotic medication", "Psychotherapy"]
 },
-"
+
 "tree_man_syndrome": {
   "type": "Genetic/Dermatological (Rare)",
   "description": "Tree man syndrome is an extremely rare disorder in which wart-like growths resembling tree bark develop on the skin due to a genetic susceptibility to human papillomavirus (HPV) infection.",
